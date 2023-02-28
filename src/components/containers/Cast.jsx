f68840432aae26255784b91a76e7d50b36ab2296
Iryna Zaitseva <zaitseva02@gmail.com>
2023-02-28 21:43:25 +0200

    work search btn

--- src/components/containers/Cast.jsx
+++ src/components/containers/Cast.jsx
@@ -23,7 +23,7 @@ export default function Cast() {
                         <li key={x.id}>
                             {x.profile_path
                                 ? <img src={imageUrl(x.profile_path)} alt="actor's pic" width={90} />
-                                : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/No_picture_available.png/401px-No_picture_available.png" alt="no pic" width={90}/>
+                                : <img src="https://rb.gy/ycrvka" alt="no pic" width={90}/>
                             }
                             <h4>{x.name}</h4>
                             <p>Character: {x.character}</p>
