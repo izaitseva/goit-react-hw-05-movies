c2af5a19d9ecb43a2f847c3b12fe5dceba01414a
Iryna Zaitseva <zaitseva02@gmail.com>
2023-02-28 21:43:57 +0200

    btn work

--- src/components/API/moviesAPI.jsx
+++ src/components/API/moviesAPI.jsx
@@ -3,11 +3,12 @@ import axios from "axios";
 const URL = `https://api.themoviedb.org/3`
 const TOP_RATED_URL = "https://api.themoviedb.org/3/movie/top_rated";
 const KEY = "c86b3c21f50eab5c7ad4f0c864f19af2";
-const SEARCH_URL = ` ${URL}/search/multi?api_key=${KEY}`;
+const SEARCH_URL = `${URL}/search/movie?api_key=${KEY}`;
+
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
-export const fetchSearchMovies = () => {
-    return axios.get(`${SEARCH_URL}`);
+export const fetchSearchMovies = (movieSearch) => {
+    return axios.get(`${SEARCH_URL}&query=${movieSearch}`);
 }
 
 export const fetchTopRatedMovies = () => {
--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,12 +1,52 @@
+import { fetchSearchMovies } from "components/API/moviesAPI";
+import { useState } from "react"
+
 export default function Movies() {
+
+    const [movieSearch, setMovieSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleChangeSearch = e => {
+        setMovieSearch(e.target.value.toLowerCase());
+    }
+
+    const handleSearch = () => {
+
+        if (movieSearch.trim() === '') {
+            return;
+        }
+
+        fetchSearchMovies(movieSearch)
+            .then(res => {
+                const movies = res.data.results;
+
+                console.log(movies);
+                setSearchResults(movies);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     return (
         <div>
             <div>
-                <input placeholder="Let's find a movie for you"></input>
-                <button>Search</button>
-            </div>
+                <input placeholder="Let's find a movie for you" onChange={handleChangeSearch} value={movieSearch}></input>
+                <button type="button" onClick={handleSearch}>Search</button>
+        
 
+                <ul>
+                    {
+                        searchResults.map(movie => {
+                            return (
+                                <li key={movie.id}>
+                                    <h2>{movie.title}</h2>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
