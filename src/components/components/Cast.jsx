9b71b14dfa69316ec3be5cf7525bcb3249970353
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-06 11:19:53 +0200

    ref cast

--- src/components/components/Cast.jsx
+++ src/components/components/Cast.jsx
@@ -5,33 +5,40 @@ import { useParams } from "react-router-dom";
 
 export default function Cast() {
 
-    const params = useParams();
-    const { movieId } = params;
+    const { movieId } = useParams();
     const [cast, setCast] = useState([]);
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        getCast(movieId)
-            .then(({ data: { cast } }) => {
-                setCast(cast) 
-            })
-            .catch(() => {
-                setError(true)
-            })
+        const fetchCast = async () => {
+            try {
+                setLoading(true);
+                const { data: { cast } } = await getCast(movieId)
+                setCast(cast)
+            } catch {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+
+        }
+        fetchCast()
     }, [movieId])
 
     return (
         <div>
             {error && <h2>There's nothing to see here</h2>}
+            {loading && <h2>Please wait...</h2>}
             <ul>
-                {cast?.map(x => (
-                    <li key={x.id}>
-                        {x.profile_path
-                            ? <img src={imageUrl(x.profile_path)} alt="actor's pic" width={90} />
+                {cast?.map(cast => (
+                    <li key={cast.id}>
+                        {cast.profile_path
+                            ? <img src={imageUrl(cast.profile_path)} alt="actor's pic" width={90} />
                             : <img src="https://rb.gy/ycrvka" alt="no pic" width={90} />
                         }
-                        <h4>{x.name}</h4>
-                        <p>Character: {x.character}</p>
+                        <h4>{cast.name}</h4>
+                        <p>Character: {cast.character}</p>
                     </li>
                 ))}
             </ul>
